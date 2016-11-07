47b50acc99de33c34c5b96e1710b75be183995f1
Neha Gandhi <Neha.Gandhi@brainloop.com>
2016-11-07 15:47:46 +0100

    first commit for text input component

---
+++ text-input.jsx
@@ -0,0 +1,12 @@
+//helloworld.js
+import React from 'react';
+class TextInput extends React.Component {
+  render() {
+    return (
+      <div> 
+            <input type='text' />
+       </div>
+    );
+  }
+}
+export default TextInput;
